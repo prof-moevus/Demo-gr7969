064d4d56aff4fd6100b40b217970d0a88273edb0
Antoine F Moevus <2613844+antmoev@users.noreply.github.com>
2025-11-25 19:11:00 -0500

    Change the cake emoji narrative

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -27,7 +27,7 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(binding.appBarMain.toolbar)
 
         binding.appBarMain.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Send a cake 🍰!", Snackbar.LENGTH_LONG)
+            Snackbar.make(view, "Send cake 🍰!", Snackbar.LENGTH_LONG)
                 .setAction("Action", null)
                 .setAnchorView(R.id.fab).show()
         }
